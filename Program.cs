caffc84143a523e97f32c1a839709a0b66c7c4cc
Tejassonar <sonartejas908@gmail.com>
2021-09-10 10:31:42 +0530

    Added Emp Wage Computation Problem Solution

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace day04
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1_EmployeeAttendance.GetAttendance();
+            //UC2_EmpWage.EmpWage();
+            //UC3_PartTimeEmpWage.PartTimeEmpWage();
+            //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            //UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            //UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("reliance",200,30,20);
+
+        }
+    }
+}
