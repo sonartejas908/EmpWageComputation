6ac962f8e905c9e7405291e737e08dc0c8736a77
Tejassonar <sonartejas908@gmail.com>
2021-09-16 14:48:11 +0530

    Added last UC

---
+++ Final_Solution/IComputeEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day04.Final_Solution
+{
+    interface IComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+        void computeEmpWage();
+
+        int getTotalWage(string company);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using day04.Final_Solution;
+using day04.UC10;
+using day04.UC9;
+using System;
 
 namespace day04
 {
@@ -15,6 +18,30 @@ namespace day04
             //UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
             //UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("reliance",200,30,20);
 
+
+            #region UC9
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+            #endregion
+
+            #region UC10
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            #endregion
+            
+
+            #region FinalSolution
+            EmpWageBuilder empWageBuild = new EmpWageBuilder();
+            empWageBuild.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuild.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuild.computeEmpWage();
+            #endregion
         }
     }
 }
